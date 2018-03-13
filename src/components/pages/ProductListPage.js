a7cda9ad98dbb8ad48241b5cb6a6fadb31f11117
alehuo <aleksi.huotala@helsinki.fi>
2018-03-13 14:32:12 +0200

    Add Product list component

--- src/components/pages/ProductListPage.js
+++ src/components/pages/ProductListPage.js
@@ -1,9 +1,67 @@
 import React, { Component } from 'react';
+import ProductList from './../sections/ProductList';
+
+const products = [
+    {
+        id: 1,
+        name: 'Twix'
+    },
+    {
+        id: 2,
+        name: 'Tupla'
+    },
+    {
+        id: 3,
+        name: 'Mars'
+    },
+    {
+        id: 4,
+        name: 'Snickers'
+    },
+    {
+        id: 5,
+        name: 'Toblerone'
+    },
+    {
+        id: 6,
+        name: 'Dacapo'
+    },
+    {
+        id: 7,
+        name: 'Jim'
+    },
+    {
+        id: 8,
+        name: 'Marianne'
+    },
+    {
+        id: 9,
+        name: 'Milka'
+    },
+    {
+        id: 10,
+        name: 'Oreo'
+    }
+];
 
 export class ProductListPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeProduct: null
+        };
+    }
+
+    onProductClick = (productId) => {
+        this.setState({ activeProduct: productId });
+    }
     render() {
         return (
-            <h1>Products</h1>
+            <ProductList
+                products={products}
+                active={this.state.activeProduct}
+                onProductClick={this.onProductClick}
+            />
         );
     }
 }
