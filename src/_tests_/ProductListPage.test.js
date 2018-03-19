b8bb51be47fff39c2ea6521b393e66690a3f7b6b
alehuo <aleksi.huotala@helsinki.fi>
2018-03-19 15:12:37 +0200

    Modify test to use the real store. Should we use this in the other tests too?

--- src/_tests_/ProductListPage.test.js
+++ src/_tests_/ProductListPage.test.js
@@ -4,19 +4,8 @@ import { mount, shallow } from 'enzyme';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import { ProductList } from '../components/sections/ProductList';
-
-const mockStore = configureStore([])({
-    authentication: {
-        isAuthenticated: true
-    },
-    product: {
-        products: [],
-        selectedProduct: null
-    },
-    productMargin: {
-        productMargin: 1.08
-    }
-});
+import store from './../store';
+const mockStore = store;
 
 describe('Product list page', () => {
     it('renders without crashing', () => {
