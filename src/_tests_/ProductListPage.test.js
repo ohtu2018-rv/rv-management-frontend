7f842521e99a5f16f54eabf8f8922b1dff01b6ee
alehuo <aleksi.huotala@helsinki.fi>
2018-03-19 14:25:21 +0200

    Modify Product list page test

--- src/_tests_/ProductListPage.test.js
+++ src/_tests_/ProductListPage.test.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import { ProductListPage } from '../components/pages/ProductListPage';
 import { mount, shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+
+const mockStore = configureStore([])({
+    authentication: {
+        isAuthenticated: false
+    },
+    product: {
+        products: []
+    },
+    productMargin: {
+        productMargin: 1
+    }
+});
 
 it('renders without crashing', () => {
-    mount(<ProductListPage/>);
+    mount(
+        <Provider store={mockStore}>
+            <ProductListPage />
+        </Provider>
+    );
 });
