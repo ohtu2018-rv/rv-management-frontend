ee6f40ff8cf637df918702d6d79d4e1319fb9f34
StarkasteBamse <sulevi.sihvola@gmail.com>
2018-04-04 11:56:51 +0300

    changed product service to send sell and buy values in cents instead of euros

--- src/services/productService.js
+++ src/services/productService.js
@@ -15,15 +15,19 @@ const addStock = (token, product) => {
     return axios
         .post(`${process.env.REACT_APP_BACKEND_URL}/${targetUrl}/product/${product.id}`,
             {
-                buyprice: product.buyprice,
+                buyprice: product.buyprice * 100,
                 margin: product.margin,
-                sellprice: product.sellprice,
+                sellprice: product.sellprice * 100,
                 quantity: product.quantity,
             },
             {
                 headers: { Authorization: 'Bearer ' + token }
             })
-        .then(res => res.data);
+        .then(res => product = {
+            ...res.data,
+            buyprice: res.data.buyprice / 100,
+            sellprice: res.data.sellprice / 100            
+        });
 };
 
 export default {
